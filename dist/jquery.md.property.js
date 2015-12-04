023c9db609254f03fb21547cbbe06d272faea850
Kurt Maine <kurt@develohost.com>
2015-12-03 23:00:53 -0600

    Rewrite of the plugins structure

--- dist/jquery.md.property.js
+++ dist/jquery.md.property.js
@@ -6,68 +6,121 @@
  *  Made by 
  *  Under ISC License
  */
-;(function ( $, window, document, undefined ) {
+;(function($) {
 
 	"use strict";
 
-	var pluginName = "mdProperty",
-		urlLookup = "https://property.melissadata.net/v3/REST/Service.svc/doLookup",
-		urlVerify = "https://personator.melissadata.net/v3/WEB/ContactVerify/doContactVerify",
-		defaults = {
-			userId: "",
-			title: "Property Lookup",
-			addressStreet: "",
-			addressCity: "",
-			addressState: "",
-			addressZip: ""
+	$.mdPropertyService = (function() {
+		
+		// Private
+		var urlLookup = "https://property.melissadata.net/v3/REST/Service.svc/doLookup",
+				urlVerify = "https://personator.melissadata.net/v3/WEB/ContactVerify/doContactVerify";
+
+			prepare_options = function(options, callback) {
+				// If only callback is passed
+				if ($.isFunction(options)) {
+					options = {
+						callback: options
+					};
+				}
+
+				options = $.extend({}, $.mdPropertyService.defaultOptions, options);
+
+				// If both options and callback are passed in
+				if ($.isFunction(callback)) {
+					options.callback = callback;
+				}
+
+				return options;
+			},
+
+			parameters = function(options) {
+				var params = {};
+
+				if (options.UserId !== "") {
+					params.userId = options.userId;
+					console.error("You must provide a User ID");
+				}
+
+				return $.param(params);
+			},
+
+			verify = function(options) {
+
+				var params = parameters(options);
+
+				url = base_url + url; // Add base prefix
+
+				if (params.length) {
+					userId = [userId, params].join("?"); // Add parameters if present
+					addressLine1 = [addressLine1, params].join("?");
+					addressLine2 = [addressLine2, params].join("?");
+					addressCity = [addressCity, params].join("?");
+					addressState = [addressState, params].join("?");
+					addressPostal = [addressPostal, params].join("?");
+				}
+
+				$.get(urlVerify, {
+					t: "mdPropertyService Request",
+					id: userId,
+					act: "Check",
+					//cols: "",
+					//opt: "",
+					//full: "",
+					//comp: "",
+					a1: addressLine1,
+					a2: addressLine2,
+					city: addressCity,
+					state: addressState,
+					postal: addressPostal
+					//ctry: "",
+					//email: "",
+					//phone: "",
+					//ff: ""
+				}, function(data) {
+					if (!data) {
+						// Exception handling for when not data is returned
+						options.callback(false, $.mdPropertyService.errors.NoDataError);
+						return;
+					}
+
+					try {
+						return options.callback(jQuery.parseXML(data));
+					} catch (e) {
+						options.callback(false, new $.mdPropertyService.errors.InvalidXML(e));
+					}
+				}, "text");
+			};
+
+		// Public
+		return {
+			defaultOptions: {
+				userId: "", // You must supply your Melissa Data user ID
+				addressLine1: "", // Line 1 of the street address
+				addressLine2: "", // Line 2 of the street address
+				addressCity: "", // The city the property is in
+				addressState: "", // The state the property is in
+				addressPostal: "" // The five digit postal (zip) code for the property
+			},
+
+			errors: {
+				NoDataError: {
+					type: "NoDataError",
+					message: "No data was supplied to the callback."
+				},
+				InvalidXML: function(data) {
+					return {
+						type: "InvalidXML",
+						message: "Invalid XML was returned. \n" + data
+					};
+				}
+			},
+
+			lookup: function(options, callback) {
+				options = prepare_options(options, callback);
+				return verify(options);
+			}
 		};
+	}());
 
-	// The actual plugin constructor
-	function Plugin ( element, options ) {
-			this.element = element;
-			this.settings = $.extend( {}, defaults, options );
-			this._defaults = defaults;
-			this._name = pluginName;
-			this.init();
-	}
-
-	// Avoid Plugin.prototype conflicts
-	$.extend(Plugin.prototype, {
-			init: function () {
-
-			},
-			
-			verify: function() {
-				$.ajax({
-					type: "GET",
-					url: urlVerify,
-					dataType: "xml",
-					success: function(xml) {
-
-					}
-				});
-			},
-			
-			lookup: function () {
-				$.ajax({
-					type: "GET",
-					url: urlLookup,
-					dataType: "xml",
-					success: function(xml) {
-
-					}
-				});
-			},
-	});
-
-	// A really lightweight plugin wrapper around the constructor,
-	// preventing against multiple instantiations
-	$.fn[ pluginName ] = function ( options ) {
-			return this.each(function() {
-					if ( !$.data( this, "plugin_" + pluginName ) ) {
-							$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
-					}
-			});
-	};
-
-})( jQuery, window, document );
+}(jQuery));
